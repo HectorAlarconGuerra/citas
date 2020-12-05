172b9dfad6936161bd885de0be32c560c8b758ce
hectoralarcon <hectoralarconguerra@gmail.com>
2020-12-05 18:01:13 -0500

    Aprendizaje estilos

--- componentes/Cita.js
+++ componentes/Cita.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const Cita = ({item}) => {
   return (
-    <View>
+    <View style={styles.cita}>
       <View>
-        <Text>Paciente: </Text>
-        <Text>{item.paciente}</Text>
+        <Text style={styles.label}>Paciente: </Text>
+        <Text style={styles.texto}>{item.paciente}</Text>
       </View>
       <View>
-        <Text>Propietario: </Text>
-        <Text>{item.propietario}</Text>
+        <Text style={styles.label}>Propietario: </Text>
+        <Text style={styles.texto}>{item.propietario}</Text>
       </View>
       <View>
-        <Text>Sintomas: </Text>
-        <Text>{item.sintomas}</Text>
+        <Text style={styles.label}>Sintomas: </Text>
+        <Text style={styles.texto}>{item.sintomas}</Text>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  cita: {
+    backgroundColor: '#FFF',
+    borderBottomColor: '#e1e1e1',
+    borderStyle: 'solid',
+    borderBottomWidth: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 20,
+  },
+  texto: {
+    fontSize: 18,
+  },
+});
+
 export default Cita;
